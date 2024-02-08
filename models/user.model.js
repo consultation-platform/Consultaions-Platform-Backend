53af00f2b0bb5ce9dfada1b49b428d518f778962
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2024-02-08 21:17:25 +0200

    feat: implement user model

---
+++ models/user.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "name required"],
+    },
+    email: {
+      type: String,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "phone number required"],
+    },
+    emailVerifyCode: String,
+    emailVerifyExpiers: Date,
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    password: {
+      type: String,
+      required: [true, "password required"],
+      minlength: [6, "Too short password"],
+    },
+    passwordChangedAt: Date,
+    passwordResetCode: String,
+    passwordResetExpires: Date,
+    passwordResetVerified: Boolean,
+
+    role: {
+      type: String,
+      enum: ["user", "mentor", "manager"],
+      default: "user",
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+   
+    address: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  // Hashing user password
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
