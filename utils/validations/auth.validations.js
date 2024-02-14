7a79a4923fbadeb876e1591813c55a0f20337e02
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2024-02-14 20:15:45 +0200

    feat : implement the user and auth validations

---
+++ utils/validations/auth.validations.js
@@ -0,0 +1,81 @@
+const slugify = require("slugify");
+const { check } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validator");
+const User = require("../../models/user.model");
+
+exports.signupValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("User required")
+    .isLength({ min: 3 })
+    .withMessage("Too short User name")
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+
+  check("phone")
+    .notEmpty()
+    .withMessage("Mobile phone is required")
+    .custom((val) =>
+      User.findOne({ phone: val }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("This mobile phone allready used "));
+        }
+      })
+    ),
+
+  check("password")
+    .notEmpty()
+    .withMessage("Password required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters")
+    .custom((password, { req }) => {
+      if (password !== req.body.passwordConfirm) {
+        throw new Error("Password Confirmation incorrect");
+      }
+      return true;
+    }),
+
+  check("passwordConfirm")
+    .notEmpty()
+    .withMessage("Password confirmation required"),
+
+  validatorMiddleware,
+];
+
+exports.loginValidator = [
+  check("phone")
+    .notEmpty()
+    .withMessage("phone number required")
+    .isMobilePhone()
+    .withMessage("Invalid mobile phone number")
+   ,
+  check("password")
+    .notEmpty()
+    .withMessage("Password required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+
+  validatorMiddleware,
+];
+
+exports.resetePasswordValidator = [
+  check("password")
+    .notEmpty()
+    .withMessage("Password required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters")
+    .custom((password, { req }) => {
+      if (password !== req.body.passwordConfirm) {
+        throw new Error("Password Confirmation incorrect");
+      }
+      return true;
+    }),
+
+  check("passwordConfirm")
+    .notEmpty()
+    .withMessage("Password confirmation required"),
+
+  validatorMiddleware,
+];
---
+++ utils/validations/user.validations.js
@@ -0,0 +1,34 @@
+const slugify = require("slugify");
+const bcrypt = require("bcryptjs");
+const { check, body } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validator");
+const User = require("../../models/userModel");
+
+exports.checkMongoID = [
+  check("id").isMongoId().withMessage("Invalid User id format"),
+  validatorMiddleware,
+];
+
+exports.updateLoggedUserValidator = [
+  body("name")
+    .optional()
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  check("email")
+    .optional()
+    .custom((val) =>
+      User.findOne({ email: val }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("E-mail already in user"));
+        }
+      })
+    ),
+  check("phone")
+    .optional()
+    .isMobilePhone(["ar-EG", "ar-SA"])
+    .withMessage("Invalid phone number only accepted Egy and SA Phone numbers"),
+
+  validatorMiddleware,
+];
