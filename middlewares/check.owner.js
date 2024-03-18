d44d7f317a4cffa4d4bd0b257929415b49a9b5fa
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-03-18 14:24:36 +0200

    feat : implement two middlewares to check the ownership of video and course

---
+++ middlewares/check.owner.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+const ApiError = require("../utils/api.error");
+const Course = require("../models/course.model");
+// const Mentor = require("../models/Mentor.model");
+
+exports.checkOwner = asyncHandler(async (req, res, next) => {
+  let courseID = {};
+  if (req.body.course) {
+    courseID = req.body.course;
+  } else if (req.params.id) {
+    courseID = req.params.id;
+  } else {
+    return next(new ApiError(`the course id is not provided`));
+  }
+  const course = await Course.findById(courseID);
+  if (!course) {
+    return next(
+      new ApiError(`the course for this id ${req.params.id} is not exist`)
+    );
+  }
+
+  if (course.owner.toString() !== req.user.id && req.user.role !== "manager") {
+    return next(new ApiError(`you are not the owner of this course`));
+  }
+
+  next();
+});
---
+++ middlewares/check.video.owner.js
@@ -0,0 +1,12 @@
+const Video = require("../models/video.model");
+const Course = require("../models/course.model");
+
+exports.checkVideoOwner = async (req, res, next) => {
+  const video = await Video.findById(req.params.id);
+  const course = await Course.findById(video.course.toString());
+  console.log(video, course);
+  if (course.owner.toString() !== req.user.id && req.user.role !== "manager") {
+    return next(new ApiError(`you are not the owner of this course`));
+  }
+  next();
+};
