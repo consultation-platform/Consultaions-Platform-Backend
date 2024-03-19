a438e7e3538704c577444c4d17c2f38787ff186a
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2024-03-19 23:35:26 +0200

    feat: implement update password and update role features

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,16 +6,18 @@ const {
   getLoggedUserData,
   updateLoggedUserData,
   getUsers,
+  updateLoggedUserPassword,
+  updateUserRole,
 } = require("../services/user.service");
 
-const {
-  updateLoggedUserValidator,
-} = require("../utils/validations/user.validations");
+// const {} = require("../utils/validations/user.validations");
 const { allowedTo, protect } = require("../services/auth.service");
 router.use(protect);
 
 router.get("/", getUsers);
 router.get("/me", getLoggedUserData);
-router.put("/updateMe", updateLoggedUserValidator, updateLoggedUserData);
+router.put("/updateMe", updateLoggedUserData);
+router.put("/update-password", updateLoggedUserPassword);
+router.put("/update-role", updateUserRole);
 
 module.exports = router;
