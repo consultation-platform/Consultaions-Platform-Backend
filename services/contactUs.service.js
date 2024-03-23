c1c0c3bfe56a1eea21c36a6ad58f8edf54239da0
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2024-03-23 16:00:50 +0200

    chore: define the retrived fields in contact forms

--- services/contactUs.service.js
+++ services/contactUs.service.js
@@ -8,7 +8,7 @@ exports.deleteContactForm = factory.deleteOne(ContactForm);
 exports.getContactFormById = factory.getOne(ContactForm);
 
 exports.getAllContactForms = asyncHandler(async (req, res, next) => {
-  const document = await ContactForm.find().select("name email phone address");
+  const document = await ContactForm.find()
   if (!document) next(new ApiError(`Error Happend `, 404));
   if (document.length === 0) {
     res.status(200).json({ message: "There Is NO Data To Retrive" });
