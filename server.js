e0d3f1263cdc2821832981f5e72ba665ddee64c5
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2024-03-23 03:58:28 +0200

    refactor: update the comments and make it a separated service

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const honorBoardRoutes = require("./routes/honor.board.routes");
 const coursesRoutes = require("./routes/courses.routes.js");
 const videosRoutes = require("./routes/video.routes.js");
 const consTicketRoutes = require("./routes/cons.tickets.routes.js");
+const commentRoutes = require("./routes/comments.routes.js");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -54,6 +55,7 @@ app.use("/api/honor-board", honorBoardRoutes);
 app.use("/api/courses", coursesRoutes);
 app.use("/api/videos", videosRoutes);
 app.use("/api/cons-tickets", consTicketRoutes);
+app.use("/api/comments", commentRoutes);
 
 // Global error handling middleware for express
 app.all("*", (req, res, next) => {
