738889adeca357d2daa6dfd06a097861447a9b93
Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>
2024-03-31 00:35:31 +0200

    chore: remove fields in the Q and A

--- services/question.service.js
+++ services/question.service.js
@@ -1,5 +1,7 @@
 const factory = require("./handlers.factory");
-const Question = require("../models/question.model")
+const Question = require("../models/question.model");
+const asyncHandler = require("express-async-handler");
+const ApiError = require("../utils/api.error");
 
 exports.createQuestion = factory.createOne(Question);
 
@@ -7,6 +9,20 @@ exports.deleteQuestion = factory.deleteOne(Question);
 
 exports.getQuestionById = factory.getOne(Question);
 
-exports.getAllQuestions = factory.getAll(Question);
+exports.getAllQuestions = asyncHandler(async (req, res, next) => {
+  const document = await Question.find().select(
+    "title image description price  answer "
+  );
+  if (!document) next(new ApiError(`Error Happend `, 404));
+  if (document.length === 0) {
+    res.status(200).json({ message: "There Is NO Data To Retrive" });
+  } else {
+    res.status(200).json({
+      message: "Documents retrieved successfully",
+      length: document.length,
+      document,
+    });
+  }
+});
 
 exports.updateQuestion = factory.updateOne(Question);
