8975766d80f05f7fd6e5664cbf7ab6b8cef57dbc
Mohmed Saleh1 <mohamed.s.kamaal@gmail.com>
2024-04-03 22:52:44 +0200

    feat: implement the cover model

---
+++ models/cover.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const coverSchema = new mongoose.Schema({
+    image: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+})
