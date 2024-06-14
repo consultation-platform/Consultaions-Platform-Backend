f2830f5b3a07ec1d2f934b0d170cb9347f7eb6a8
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-06-14 03:38:34 +0300

    chore: updates on header

--- services/cover.service.js
+++ services/cover.service.js
@@ -1,5 +1,8 @@
 const factory = require("./handlers.factory");
 const Cover = require("../models/cover.model");
+const asyncHandler = require("express-async-handler");
+const ApiError = require("../utils/api.error");
+
 const { uploadSingleImage } = require("../middlewares/uploadImages");
 
 exports.uploadCoverImage = uploadSingleImage("image");
@@ -12,4 +15,16 @@ exports.getCoverById = factory.getOne(Cover);
 
 exports.updateCover = factory.updateOne(Cover);
 
-exports.getAllCovers = factory.getAll(Cover);
+exports.getAllCovers = asyncHandler(async (req, res, next) => {
+  const document = await Cover.find().select("title image description  name");
+  if (!document) next(new ApiError(`Error Happend `, 404));
+  if (document.length === 0) {
+    res.status(200).json({ message: "There Is NO Data To Retrive" });
+  } else {
+    res.status(200).json({
+      message: "Documents retrieved successfully",
+      length: document.length,
+      document,
+    });
+  }
+});
